cd809f3e00e3a8bdbcaac4743e168e14c6a6d1c6
netcool <51483807+toksichny@users.noreply.github.com>
2024-09-30 23:28:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -203,15 +203,11 @@ if __name__ == "__main__":
 
     while profiles:
         random_profile = random.choice(profiles)
-        try:
-            image_url, pin_id = download_random_image(random_profile)
+        
+        image_url, pin_id = download_random_image(random_profile)
 
-            if image_url is not None and pin_id is not None:  # Проверяем, что значения не None
-                post_pin(image_url)
-            else:
-                print(f"Изображения для профиля {random_profile} уже скачаны.")
-                profiles.remove(random_profile)  # Удаляем профиль из списка
-
-        except Exception as e:
-            print(f"Произошла ошибка при обработке профиля {random_profile}: {e}")
-            profiles.remove(random_profile)  # Удаляем профиль из списка при ошибке
+        if image_url is not None and pin_id is not None:  # Проверяем, что значения не None
+            post_pin(image_url)
+        else:
+            print(f"Изображения для профиля {random_profile} уже скачаны.")
+            profiles.remove(random_profile)  # Удаляем профиль из списка
